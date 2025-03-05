8d32a3020d094ef5a2309f3f8717ab4f3ed9c85b
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-03-05 22:39:35 +0300

    smartnet/message.py: working on class

--- smartnet/message.py
+++ smartnet/message.py
@@ -1,10 +1,8 @@
 '''
-Created on 4 мар. 2025 г.
-
 @author: admin
 '''
 
-from canbus import canbus
+import can
 
 
 class Message(object):
@@ -13,10 +11,49 @@ class Message(object):
 	'''
 
 
-	def __init__(self, params):
+	def __init__(self, programType, programId, functionId, request, data):
 		'''
 		Constructor
 		'''
+		self.programId   = programId
+		self.programType = programType
+		self.functionId  = functionId
+		self.request     = request
+		self.data        = data
+	
+	def generateHeader(self):
+		if self.request:
+			flag = 0x00
+		else:
+			flag = 0x10
+
+		byte0 = self.programType
+		byte1 = self.programId
+		byte2 = self.functionId
+		byte3 = flag
+
+		header = (
+			(byte0 <<  0) |
+			(byte1 <<  8) |
+			(byte2 << 16) |
+			(byte3 << 24))
+
+		return header
+
+	def send(self):
+		header = self.generateHeader()
+
 		
-		canbus.sendMessage()
-		
+		msg = can.Message(
+			arbitration_id=header,
+			data=self.data,
+			is_extended_id=True
+		)
+
+		#bus config loaded from ~/can.conf file
+		#if you don't have it you should make one
+		#https://python-can.readthedocs.io/en/stable/configuration.html
+		with can.Bus() as bus:
+			bus.send(msg)
+			
+			
