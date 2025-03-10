21e67254740fb859fa3454f921d34e9e3b7cb0b5
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-03-10 21:26:27 +0300

    controllers/defaultPreset.py: working on preset

--- controllers/defaultPreset.py
+++ controllers/defaultPreset.py
@@ -1,5 +1,9 @@
 
 from controllers.channelMapping import ChannelMapping as Mapping
+from controllers.presetMapping import HeatingCircuitInputMapping  as hcInputMapping
+from controllers.presetMapping import HeatingCircuitOutputMapping as hcOutputMapping
+from controllers.presetMapping import RoomInputMapping  as roomInputMapping
+from controllers.presetMapping import RoomOutputMapping as roomOutputMapping
 
 import smartnet.constants as snc
 import controllers.preset
@@ -73,24 +77,28 @@ programSettings = {
 	'OUTDOOR_SENSOR'    : None,
 }
 
+def inputMapping (id): return Mapping(id, 'CHANNEL_SENSOR', getHostId())
+def outputMapping(id): return Mapping(id, 'CHANNEL_RELAY' , getHostId())
+
+
 programInputs = {
-	'HEATING_CIRCUIT_1' : [Mapping(0, 'CHANNEL_SENSOR', getHostId())],
-	'HEATING_CIRCUIT_2' : [Mapping(1, 'CHANNEL_SENSOR', getHostId())],
-	'ROOM_DEVICE_1'     : [Mapping(2, 'CHANNEL_SENSOR', getHostId())],
-	'ROOM_DEVICE_2'     : [Mapping(3, 'CHANNEL_SENSOR', getHostId())],
-	'DHW'               : [Mapping(4, 'CHANNEL_SENSOR', getHostId())],
+	'HEATING_CIRCUIT_1' : hcInputMapping(temperature = inputMapping(0)).get(),
+	'HEATING_CIRCUIT_2' : hcInputMapping(temperature = inputMapping(1)).get(),
+	'ROOM_DEVICE_1'     : roomInputMapping(roomTemperature = inputMapping(2)).get(),
+	'ROOM_DEVICE_2'     : roomInputMapping(roomTemperature = inputMapping(3)).get(),
+	'DHW'               : hcInputMapping(temperature = inputMapping(4)).get(),
 	'BOILER'            : None,
-	'CASCADE_MANAGER'   : [Mapping(6, 'CHANNEL_SENSOR', getHostId())],
-	'OUTDOOR_SENSOR'    : [Mapping(7, 'CHANNEL_SENSOR', getHostId())],
+	'CASCADE_MANAGER'   : hcInputMapping(temperature = inputMapping(6)).get(),
+	'OUTDOOR_SENSOR'    : hcInputMapping(temperature = inputMapping(7)).get(),
 }
 
 programOutputs = {
-	'HEATING_CIRCUIT_1' : [Mapping(0, 'CHANNEL_RELAY', getHostId())],
-	'HEATING_CIRCUIT_2' : [Mapping(1, 'CHANNEL_RELAY', getHostId())],
+	'HEATING_CIRCUIT_1' : hcOutputMapping(analogValve = outputMapping(0), pump = outputMapping(1)).get(),
+	'HEATING_CIRCUIT_2' : hcOutputMapping(analogValve = outputMapping(2), pump = outputMapping(3)).get(),
 	'ROOM_DEVICE_1'     : None,
 	'ROOM_DEVICE_2'     : None,
-	'DHW'               : [Mapping(4, 'CHANNEL_RELAY', getHostId())],
-	'BOILER'            : [Mapping(5, 'CHANNEL_RELAY', getHostId())],
+	'DHW'               : [outputMapping(4)],
+	'BOILER'            : [outputMapping(5)],
 	'CASCADE_MANAGER'   : None,
 	'OUTDOOR_SENSOR'    : None,
 }
