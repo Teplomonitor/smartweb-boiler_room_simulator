ed1f5a8bf2f5914016c8daddbc395e1438d3edba
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-03-11 21:33:31 +0300

    simulator: working on boiler room simulation

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -2,11 +2,15 @@
 import threading
 import can
 import time
+import math
 
 import smartnet.constants as snc
 from smartnet.message import createBus as createBus
 from smartnet.message import Message as smartnetMessage
 from controllers.channelMapping import ChannelMapping as Mapping
+
+def TEMPERATURE(val):
+	return int(val*10)
 
 def reportSensorValue(mapping, sensorValue, bus = None):
 	value = [
@@ -30,14 +34,24 @@ class OatSimulator(threading.Thread):
 		inputs = self._preset.getInputs()
 		oatInput = inputs.getOat()
 		self._sensorMapping = oatInput
-		self._canbus = canbus
+		self._canbus        = canbus
+		self._time_start    = time.time()
+
+	def getElapsedTime(self):
+		return time.time() - self._time_start
 
 	def reportOat(self, value):
 		reportSensorValue(self._sensorMapping, value, self._canbus)
 
+	def computeOat(self):
+		pi = 3.14
+		oat = math.cos(self.getElapsedTime()/1000.0 + pi/2) * 20
+		print(f'oat = {oat}')
+		return oat
+
 	def run(self):
 		while True:
-			self.reportOat(100)
+			self.reportOat(TEMPERATURE(self.computeOat()))
 			time.sleep(5)
 
 class Simulator(can.Listener):
@@ -53,22 +67,20 @@ class Simulator(can.Listener):
 
 		self._canbus   = createBus()
 		self._notifier = can.Notifier(self._canbus, [self])
-		self._oat      = None
 
 		programsList = self._controller.getProgramsAddList()
 
 		for program in programsList:
 			if program.getType() == snc.ProgramType['OUTDOOR_SENSOR']:
-
 				oatThread = OatSimulator("OAT", program.getId(), program, self._canbus)
 				oatThread.daemon = True
 				oatThread.start()
+				self._oat = oatThread
+				break
 
 	def on_message_received(self, message):
 		if message is None:
 			return
-
-
 
 	def run(self):
 		while True:
