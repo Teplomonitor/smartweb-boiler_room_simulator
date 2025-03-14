11d78d2a06bc708fcae985b8c9da4039d7cc23bb
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-03-14 14:37:57 +0300

    simulator/heating_circuit.py: working on sim

--- simulator/heating_circuit.py
+++ simulator/heating_circuit.py
@@ -2,8 +2,12 @@
 import threading
 import math
 import time
+
 from smartnet.units import TEMPERATURE as TEMPERATURE
 from simulator.sensorReport import reportSensorValue as reportSensorValue
+
+def limit(lower_bound, value, upper_bound):
+	return max(min(value, upper_bound), lower_bound)
 
 class Simulator(threading.Thread):
 	def __init__(self, thread_name, thread_ID, program, canbus, control):
@@ -14,14 +18,100 @@ class Simulator(threading.Thread):
 		self._preset     = self._program.getPreset()
 		self._canbus        = canbus
 		self._time_start    = time.time()
+		self._control    = control
 		
-		inputs = self._preset.getInputs()
-		oatInput = inputs.getTemperature()
-		self._sensorMapping = oatInput
+		self._outputId = {
+			'analogValve'         : 0,
+			'tptValveOpen'        : 1,
+			'tptValveClose'       : 2,
+			'pump'                : 3,
+			'thermomotor'         : 4,
+			'heatchangePump'      : 5,
+			'analogPump'          : 6,
+		}
+
+		self._inputId = {
+			'temperature'         : 0,
+			'thermostat'          : 1,
+			'outsideRequest'      : 2,
+			'pumpControl'         : 3,
+			'backwardTemperature' : 4,
+		}
+
+		self.setTemperature(20)
+
+
+	def getTemperature(self):
+		return self._program.getInput(self._inputId['temperature']).getValue()
+
+	def setTemperature(self, value):
+		print(f'circuit: {value}')
+		self._program.getInput(self._inputId['temperature']).setValue(value)
 
 	def getElapsedTime(self):
 		return time.time() - self._time_start
 
+	def getPumpState(self):
+		pump = self._program.getOutput(self._outputId['pump'])
+		if pump.getMapping() is None:
+			return 1
+
+		if pump.getValue():
+			return 1
+
+		return 0
+
+	def getValveState(self):
+		valve = self._program.getOutput(self._outputId['analogValve'])
+		if valve.getMapping() is None:
+			return 1
+
+		valve = valve.getValue()
+		if valve is None:
+			return 1
+		return valve / 254
+
+	def getMaxPower(self):
+		return self._preset.getPower()
+
+	def getPower(self):
+		if self.getPumpState() == 0:
+			return 0
+
+		return self.getValveState()*self.getMaxPower()
+
+	def getSourceTemperature(self):
+		sourceList = self._control.getSourceList()
+		sourceId   = self._program.getPreset().getSettings().getSource()
+		for source in sourceList:
+			if source._program.getId() == sourceId:
+				return source.getTemperature()
+
+		return 60
+
+	def getHeating(self):
+		sourceTemp = self.getSourceTemperature()
+		sourceTemp = sourceTemp - 5 # we loose some temp coming from source
+
+		temp  = self.getTemperature()
+		valve = self.getValveState()
+
+		dT = sourceTemp - temp
+		return dT * valve * 0.1
+
+	def getCooling(self):
+		return -0.1 # should depend on weather and room temp
+
+	def computeTemperature(self):
+		temp  = self.getTemperature()
+
+		temp = temp + self.getHeating() + self.getCooling()
+
+		temp = limit(-30, temp, 120)
+
+		return temp
+
 	def run(self):
 		while True:
+			self.setTemperature(self.computeTemperature())
 			time.sleep(2)
