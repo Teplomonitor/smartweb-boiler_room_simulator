ce972b16029574e3ea2718bd1865df1408aff5f5
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-04-02 14:50:40 +0300

    presets/settings.py: add retry

--- presets/settings.py
+++ presets/settings.py
@@ -61,9 +61,16 @@ class RemoteControlParameter(object):
 		request        = generateRequest()
 		responseFilter = generateRequiredResponse()
 
-		response = request.send(responseFilter, 10)
-
-		return handleResponse()
+		i = 0
+		while i < 3:
+			response = request.send(responseFilter, 10)
+			result = handleResponse()
+			if result:
+				break;
+			print('retry')
+			i = i + 1
+			
+		return result
 
 
 class HeatingCircuitSettings(object):
