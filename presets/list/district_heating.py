8435276442008236aea4119bae9e54b8fdf96723
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-04-28 22:41:14 +0300

    presets/list/district_heating.py: working on new preset

--- presets/list/district_heating.py
+++ presets/list/district_heating.py
@@ -1,16 +1,28 @@
 
 from controllers.channelMapping import ChannelMapping as Mapping
 
-from presets.mapping import BoilerInputMapping          as boilerInputMapping
-from presets.mapping import BoilerOutputMapping         as boilerOutputMapping
-from presets.mapping import OatInputMapping             as oatInputMapping
-from presets.mapping import OatOutputMapping            as oatOutputMapping
-from presets.mapping import SnowMelterInputMapping      as smInputMapping
-from presets.mapping import SnowMelterOutputMapping     as smOutputMapping
+from presets.mapping import HeatingCircuitInputMapping   as hcInputMapping
+from presets.mapping import HeatingCircuitOutputMapping  as hcOutputMapping
+from presets.mapping import RoomInputMapping             as roomInputMapping
+from presets.mapping import RoomOutputMapping            as roomOutputMapping
+from presets.mapping import DhwInputMapping              as dhwInputMapping
+from presets.mapping import DhwOutputMapping             as dhwOutputMapping
+from presets.mapping import BoilerInputMapping           as boilerInputMapping
+from presets.mapping import BoilerOutputMapping          as boilerOutputMapping
+from presets.mapping import OatInputMapping              as oatInputMapping
+from presets.mapping import OatOutputMapping             as oatOutputMapping
+from presets.mapping import DistrictHeatingInputMapping  as dhInputMapping
+from presets.mapping import DistrictHeatingOutputMapping as dhOutputMapping
 
-from presets.settings import SnowMelterSettings as smSettings
+from presets.settings import HeatingCircuitSettings  as hcSettings
+from presets.settings import DhwSettings             as dhwSettings
+from presets.settings import RoomSettings            as roomSettings
+from presets.settings import DistrictHeatingSettings as dhSettings
 
 import presets.preset
+
+def getHostId():
+	return 123
 
 hostList = [
 	'HOST_1',
@@ -34,39 +46,69 @@ hostTitle = {
 
 
 programList = [
-	'SNOW_MELTER'   ,
-	'BOILER'        ,
-	'OUTDOOR_SENSOR',
+	'HEATING_CIRCUIT_1',
+	'HEATING_CIRCUIT_2',
+	'ROOM_DEVICE_1'    ,
+	'ROOM_DEVICE_2'    ,
+	'DHW'              ,
+	'BOILER'           ,
+	'OUTDOOR_SENSOR'   ,
+	'DISTRICT_HEATING' ,
 ]
 
 programType = {
-	'SNOW_MELTER'    : 'SNOWMELT',
-	'BOILER'         : 'BOILER'         ,
-	'OUTDOOR_SENSOR' : 'OUTDOOR_SENSOR' ,
+	'HEATING_CIRCUIT_1' : 'HEATING_CIRCUIT',
+	'HEATING_CIRCUIT_2' : 'HEATING_CIRCUIT',
+	'ROOM_DEVICE_1'     : 'ROOM_DEVICE'    ,
+	'ROOM_DEVICE_2'     : 'ROOM_DEVICE'    ,
+	'DHW'               : 'DHW'            ,
+	'BOILER'            : 'BOILER'         ,
+	'OUTDOOR_SENSOR'    : 'OUTDOOR_SENSOR' ,
+	'DISTRICT_HEATING'  : 'DISTRICT_HEATING',
 }
 
 programScheme = {
-	'SNOW_MELTER'     : 'DEFAULT',
-	'BOILER'          : 'DEFAULT',
-	'OUTDOOR_SENSOR'  : 'DEFAULT',
+	'HEATING_CIRCUIT_1' : 'CIRCUIT_MIXED' ,
+	'HEATING_CIRCUIT_2' : 'CIRCUIT_MIXED' ,
+	'ROOM_DEVICE_1'     : 'DEFAULT'       ,
+	'ROOM_DEVICE_2'     : 'DEFAULT'       ,
+	'DHW'               : 'DEFAULT'       ,
+	'BOILER'            : 'DEFAULT'       ,
+	'OUTDOOR_SENSOR'    : 'DEFAULT'       ,
+	'DISTRICT_HEATING'  : 'DEFAULT'       ,
 }
 
 programTitle = {
-	'SNOW_MELTER'     :'snowmelt',
-	'BOILER'          :'Boiler1' ,
-	'OUTDOOR_SENSOR'  :'OAT'     ,
+	'HEATING_CIRCUIT_1' :'Circ1'   ,
+	'HEATING_CIRCUIT_2' :'Circ2'   ,
+	'ROOM_DEVICE_1'     :'Room1'   ,
+	'ROOM_DEVICE_2'     :'Room2'   ,
+	'DHW'               :'DHW'     ,
+	'BOILER'            :'Boiler1' ,
+	'OUTDOOR_SENSOR'    :'OAT'     ,
+	'DISTRICT_HEATING'  :'distHeat',
 }
 
 programId = {
-	'SNOW_MELTER'    : 101,
-	'BOILER'         : 102,
-	'OUTDOOR_SENSOR' : 103,
+	'HEATING_CIRCUIT_1' : 101,
+	'HEATING_CIRCUIT_2' : 102,
+	'ROOM_DEVICE_1'     : 103,
+	'ROOM_DEVICE_2'     : 104,
+	'DHW'               : 105,
+	'BOILER'            : 106,
+	'OUTDOOR_SENSOR'    : 107,
+	'DISTRICT_HEATING'  : 108,
 }
 
 programSettings = {
-	'SNOW_MELTER'   : smSettings(programId['BOILER']),
-	'BOILER'        : None,
-	'OUTDOOR_SENSOR': None,
+	'HEATING_CIRCUIT_1' : hcSettings(programId['DISTRICT_HEATING']),
+	'HEATING_CIRCUIT_2' : hcSettings(programId['DISTRICT_HEATING'], 60),
+	'ROOM_DEVICE_1'     : roomSettings(None                          , programId['HEATING_CIRCUIT_1']),
+	'ROOM_DEVICE_2'     : roomSettings(programId['HEATING_CIRCUIT_2'], programId['HEATING_CIRCUIT_1']),
+	'DHW'               : dhwSettings(programId['DISTRICT_HEATING']),
+	'BOILER'            : None,
+	'OUTDOOR_SENSOR'    : None,
+	'DISTRICT_HEATING'  : dhSettings(programId['BOILER']),
 }
 
 def inputMapping (channel_id, host_id): return Mapping(channel_id, 'CHANNEL_SENSOR', host_id)
@@ -74,21 +116,45 @@ def outputMapping(channel_id, host_id): return Mapping(channel_id, 'CHANNEL_RELA
 
 
 programInputs = {
-	'SNOW_MELTER'   : smInputMapping     (inputMapping(0, hostId['HOST_1']), inputMapping(1, hostId['HOST_1']), inputMapping(2, hostId['HOST_1'])),
-	'BOILER'        : boilerInputMapping (inputMapping(3, hostId['HOST_1'])),
-	'OUTDOOR_SENSOR': oatInputMapping    (inputMapping(4, hostId['HOST_1'])),
+	'HEATING_CIRCUIT_1' : hcInputMapping     (inputMapping(0, hostId['HOST_1'])),
+	'HEATING_CIRCUIT_2' : hcInputMapping     (inputMapping(1, hostId['HOST_1'])),
+	'ROOM_DEVICE_1'     : roomInputMapping   (inputMapping(2, hostId['HOST_1'])),
+	'ROOM_DEVICE_2'     : roomInputMapping   (inputMapping(3, hostId['HOST_1'])),
+	'DHW'               : dhwInputMapping    (inputMapping(4, hostId['HOST_1'])),
+	'BOILER'            : boilerInputMapping (),
+	'OUTDOOR_SENSOR'    : oatInputMapping    (inputMapping(0, hostId['HOST_2'])),
+	'DISTRICT_HEATING'  : dhInputMapping     (
+		inputMapping(1, hostId['HOST_2']),
+		inputMapping(2, hostId['HOST_2']),
+		inputMapping(3, hostId['HOST_2']),
+		inputMapping(4, hostId['HOST_2']),
+		),
 }
 
 programOutputs = {
-	'SNOW_MELTER'   : smOutputMapping(None, outputMapping(1, hostId['HOST_1']), outputMapping(6, hostId['HOST_1'])),
-	'BOILER'        : boilerOutputMapping(pump = outputMapping(3, hostId['HOST_1']), burner1 = outputMapping(4, hostId['HOST_1'])),
-	'OUTDOOR_SENSOR': oatOutputMapping(),
+	'HEATING_CIRCUIT_1' : hcOutputMapping(analogValve = outputMapping(6, hostId['HOST_1']), pump = outputMapping(0, hostId['HOST_1'])),
+	'HEATING_CIRCUIT_2' : hcOutputMapping(analogValve = outputMapping(7, hostId['HOST_1']), pump = outputMapping(1, hostId['HOST_1'])),
+	'ROOM_DEVICE_1'     : roomOutputMapping(),
+	'ROOM_DEVICE_2'     : roomOutputMapping(),
+	'DHW'               : dhwOutputMapping(outputMapping(2, hostId['HOST_1']), outputMapping(3, hostId['HOST_1'])),
+	'BOILER'            : boilerOutputMapping(pump = outputMapping(0, hostId['HOST_2']), burner1 = outputMapping(1, hostId['HOST_2'])),
+	'OUTDOOR_SENSOR'    : oatOutputMapping(),
+	'DISTRICT_HEATING'  : dhOutputMapping(
+		outputMapping(2, hostId['HOST_2']), 
+		outputMapping(3, hostId['HOST_2']), 
+		None, 
+		outputMapping(6, hostId['HOST_2'])),
 }
 
 programPower = {
-	'SNOW_MELTER'   :  2,
-	'BOILER'        :  3,
-	'OUTDOOR_SENSOR': None,
+	'HEATING_CIRCUIT_1' : -2,
+	'HEATING_CIRCUIT_2' : -3,
+	'ROOM_DEVICE_1'     : -1,
+	'ROOM_DEVICE_2'     : -2,
+	'DHW'               : -4,
+	'BOILER'            : 15,
+	'OUTDOOR_SENSOR'    : None,
+	'DISTRICT_HEATING'  :  7,
 }
 
 def getPresetsList() :
