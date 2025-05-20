93dc66773024bbc3264fd990d6201511e6aeb90c
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-05-20 13:00:39 +0300

    smartnet/channelTitle.py: working on IO titles

---
+++ smartnet/channelTitle.py
@@ -0,0 +1,104 @@
+'''
+@author: admin
+'''
+
+InputTitleOat = [
+	'Улица',
+]
+InputTitleCascade           = [
+	'Коллектор',
+	]
+InputTitleRoomDevice        = [
+	'Т помещения',
+	'Режим',
+	'Т пола',
+	'Т стены',
+	'Влажность',
+	'CO2',
+	'Движение',
+	]
+InputTitleHeatingCircuit    = [
+	'Улица',
+	]
+InputTitleDistrictHeating   = [
+	'Улица',
+	]
+InputTitleDhw               = [
+	'Улица',
+	]
+InputTitlePool              = [
+	'Улица',
+	]
+InputTitleSnowMelt          = [
+	'Улица',
+	]
+InputTitleBoiler            = [
+	'Улица',
+	]
+InputTitleSolarCollector    = [
+	'Улица',
+	]
+InputTitleSolarGenericRelay = [
+	'Улица',
+	]
+InputTitleSolarAlarm        = [
+	'Улица',
+	]
+InputTitleSolarFillingLoop  = [
+	'Улица',
+	]
+
+OutputTitleOat               = []
+OutputTitleCascade           = []
+OutputTitleRoomDevice        = [
+	'Клапан ТП',
+	'Клапан РО',
+	'Клапан ДН',
+	'Сигнал ТП',
+	'Сигнал РО',
+	'Сигнал ДН',
+	]
+OutputTitleHeatingCircuit    = ['Улица',]
+OutputTitleDistrictHeating   = ['Улица',]
+OutputTitleDhw               = ['Улица',]
+OutputTitlePool              = ['Улица',]
+OutputTitleSnowMelt          = ['Улица',]
+OutputTitleBoiler            = ['Улица',]
+OutputTitleSolarCollector    = ['Улица',]
+OutputTitleSolarGenericRelay = ['Улица',]
+OutputTitleSolarAlarm        = ['Улица',]
+OutputTitleSolarFillingLoop  = ['Улица',]
+
+
+ProgramInputTitle = {
+	'OUTDOOR_SENSOR'   : InputTitleOat,
+	'CASCADE_MANAGER'  : InputTitleCascade          ,
+	'ROOM_DEVICE'      : InputTitleRoomDevice       ,
+	'HEATING_CIRCUIT'  : InputTitleHeatingCircuit   ,
+	'DISTRICT_HEATING' : InputTitleDistrictHeating  ,
+	'DHW'              : InputTitleDhw              ,
+	'POOL'             : InputTitlePool             ,
+	'SNOWMELT'         : InputTitleSnowMelt         ,
+	'BOILER'           : InputTitleBoiler           ,
+	'SOLAR_COLLECTOR'  : InputTitleSolarCollector   ,
+	'GENERIC_RELAY'    : InputTitleSolarGenericRelay,
+	'ALARM'            : InputTitleSolarAlarm       ,
+	'FILLING_LOOP'     : InputTitleSolarFillingLoop ,
+}
+
+ProgramOutputTitle = {
+	'OUTDOOR_SENSOR'   : OutputTitleOat              ,
+	'CASCADE_MANAGER'  : OutputTitleCascade          ,
+	'ROOM_DEVICE'      : OutputTitleRoomDevice       ,
+	'HEATING_CIRCUIT'  : OutputTitleHeatingCircuit   ,
+	'DISTRICT_HEATING' : OutputTitleDistrictHeating  ,
+	'DHW'              : OutputTitleDhw              ,
+	'POOL'             : OutputTitlePool             ,
+	'SNOWMELT'         : OutputTitleSnowMelt         ,
+	'BOILER'           : OutputTitleBoiler           ,
+	'SOLAR_COLLECTOR'  : OutputTitleSolarCollector   ,
+	'GENERIC_RELAY'    : OutputTitleSolarGenericRelay,
+	'ALARM'            : OutputTitleSolarAlarm       ,
+	'FILLING_LOOP'     : OutputTitleSolarFillingLoop ,
+}
+
