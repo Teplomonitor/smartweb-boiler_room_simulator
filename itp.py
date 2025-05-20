03f7bfe62e6c508e52081d198bf8b09f9c133977
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-05-20 17:33:00 +0300

    itp.py: update file

--- itp.py
+++ itp.py
@@ -1,31 +1,46 @@
+import math
 # начальные условия
-tintown = 70 # на входе из города - постоянная
-tinhouse0 = 24 # температура начальная в контуре отопления
-tinhouse_demand = 60 # требуемая температура подачи
-troom = 15
+tintown = 70 # на входе из города - постоянная ---------------------------
+
+tinhouse0 = 58 # температура начальная в контуре отопления ---------------
+
+tinhouse_demand = 60 # требуемая температура подачи ----------------------
+
+troom = 18 # температура в помещении, от нее зависит рассеяние энергии
+
 cw = 4200 # теплоемкость воды
-qhouse_chas = 1.5 # расход куб в час в отоплении
+
+qhouse_chas = 2 # расход куб/час в отоплении -----------------------------
 qhouse = 1.5/3.6 # расход кг/сек в доме постоянный.
+
 cwq = qhouse*cw # так короче
-qtown_chas_max = 2 # мах расход куб в час из города
+qtown_chas_max = 3 # мах расход куб/час из города ------------------------
 qtown_max = qtown_chas_max/3.6 # секундный из города
-real_time = 0
-tservak = 120 # время разворота сервака
+
+tservak = 120 # время разворота сервака ---------------------------------
 proport = 20 # полоса пропорциональности температуры
 
-btermo=500 # теплоотдача батарей НЕ трогать
-ato = 4000 # теплопередача ТО тоже не трогать
+btermo=1000 # теплоотдача батарей НЕ трогать
+ato = 3000 # теплопередача ТО тоже не трогать
 
-tau=5 # шаг по времени сек
+sqwear=1.5 # Площадь ТО в кв.м.-----------------------
+tau=3 # шаг по времени сек
 
-ugolserv = 0
+real_time = 0
+ugolserv = 1
 tinhouse = tinhouse0
 t_rettown = tintown
+i = 0
+power1 = 100
+atos=ato * sqwear
+difftold = 0
 
-while abs(tinhouse-tinhouse_demand) > 1 and tinhouse * t_rettown > 0:
-#i=1
-#while i<10:
-#    i +=1
+#while abs(tinhouse-tinhouse_demand) > 1 and tinhouse * t_rettown > 0 and i<200:
+while i < 20:
+    # не больше 200 шагов и температуры положительные 
+   
+    i +=1
+    cond = True
     x = (tinhouse_demand-tinhouse)/proport
     # расчет расхода из города
     if x>1 :
@@ -36,18 +51,33 @@ while abs(tinhouse-tinhouse_demand) > 1 and tinhouse * t_rettown > 0:
     if ugolserv > 1 : ugolserv = 1
        
     qtown = qtown_max * ugolserv # расход из города при данном угле сервака
-    #print("ugol %.2f." % ugolserv, "qtown %.3f." % qtown)
-    
-    t_rethouse = ((cwq - btermo/2)*tinhouse + btermo * troom)/(cwq+btermo/2) # обратка из дома
-    t_rettown = tintown-(ato * qtown/qtown_max)*((tintown+t_rettown)/2 - (tinhouse+t_rethouse)/2)/cw #обратка в город
-    
-    etown=tau*qtown*cw*(tintown - t_rettown)
-    ehouse=tau*qhouse*cw*(tinhouse-t_rethouse)
-    d_tinhouse = (etown-ehouse)/(cw*tau*qhouse) #подача в дом - подсчет разности энергий
-    tinhouse = tinhouse + d_tinhouse # подача в дом
-   # print('tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown, etown, ehouse)
+       
+    j=1
+    while cond:
+        j +=1
+        t_rethouse = ((cwq - btermo/2)*tinhouse + btermo * troom)/(cwq+btermo/2) # обратка из дома
+        
+        difft = (tintown+t_rettown)/2.0 - (tinhouse+t_rethouse)/2.0
+
+        t_rettown = tintown - atos * qtown * difft/qtown_max/cw #обратка в город
+            
+        etown=qtown*cw*(tintown - t_rettown)
+        ehouse=qhouse*cw*(tinhouse-t_rethouse)
+        d_tinhouse = (etown-ehouse)/(cw*qhouse) #подача в дом - подсчет разности энергий
+        tinhouse = tinhouse + d_tinhouse # подача в дом 
+
+        power1 = qtown*cw*(tintown-t_rettown) # подсчет текущей мощности теплопередачи ватт
+        
+        if abs(difft-difftold)<0.2 or j>10: # условие выхода из внутреннего цикла. не больше 10 итераций 
+            cond=False
+        difftold=difft
+        
+        print('tmp: tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown)
+        
     real_time += tau
-    power = qtown*cw*(tintown-t_rettown) # подсчет текущей мощности теплопередачи ватт
-    print('tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown,'power %.2f.'% power)
-    print('-----------------------')
+    
+    print('power town %.2f.'% power1)
+   
+    print('tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown)
+    print(i,'шаг по времени ----------------------')
 print('gameover', real_time, tau)    
