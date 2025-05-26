d77094b634a2451b91a4efbabb99355a453fff53
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-05-26 15:31:16 +0300

    scenario/scenario.py: testing pytest-io

--- scenario/scenario.py
+++ scenario/scenario.py
@@ -2,14 +2,28 @@
 @author: admin
 '''
 
+# pip install pytest-aio
+
+import asyncio
+
+
+async def test_async():
+	print('Hello ...')
+	await asyncio.sleep(10)
+	print('... World!')
+	assert False
+
 class Scenario(object):
 	'''
 	classdocs
 	'''
 
 
-	def __init__(self, params):
+	def __init__(self):
 		'''
 		Constructor
 		'''
 		
+		
+	def run(self):
+		asyncio.run(test_async())
