910839c944f9fc9cc3d8c6993584029d011fdedb
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-06-05 15:40:02 +0300

    itp.py: another logic update

--- itp.py
+++ itp.py
@@ -2,9 +2,9 @@ import math
 # начальные условия
 tintown = 70 # на входе из города - постоянная ---------------------------
 
-tinhouse0 = 24 # температура начальная в контуре отопления ---------------
+tinhouse0 = 55 # температура начальная в контуре отопления ---------------
 
-tinhouse_demand = 80 # требуемая температура подачи ----------------------
+tinhouse_demand = 45 # требуемая температура подачи ----------------------
 
 troom = 18 # температура в помещении, от нее зависит рассеяние энергии
 
@@ -25,62 +25,90 @@ btermo=1200 # теплоотдача батарей НЕ трогать
 ato = 3000 # теплопередача ТО тоже не трогать
 
 square=1 # Площадь ТО в кв.м.-----------------------
-tau=2 # шаг по времени сек
+tau=1 # шаг по времени сек
 
 real_time = 0
-ugolserv = 0
+ugolserv = 1
 tinhouse = tinhouse0
-t_rettown = tinhouse
+t_rettown = tintown-10
+print(t_rettown)
 i = 0
 atos=ato * square
 
-#--------------------------------------
-
-while abs(tinhouse-tinhouse_demand) > 1 and tinhouse * t_rettown > 0 and i<100:
+#--------------------------------------abs(tinhouse-tinhouse_demand) > 1 and tinhouse * t_rettown > 0 and
+while  i<200:
     # не больше i шагов и температуры положительные 
    
     i +=1
 
     qtown0 = qtown_max * ugolserv
     x = (tinhouse_demand-tinhouse)/proport
-    # расчет расхода из города
-    if x>1 :
-        ugolserv = ugolserv + tau/tservak
+    # расчет расхода из города ----------------------
+    if abs(x)>1 :
+        ugolserv = ugolserv + (x/abs(x))*tau/tservak
     else :
         ugolserv = ugolserv + x*tau/tservak
     if ugolserv < 0 : ugolserv = 0
     if ugolserv > 1 : ugolserv = 1
        
-    qtown = qtown_max * ugolserv # расход из города при данном угле сервака
+    qtown = qtown_max * ugolserv # расход из города при данном угле сервака 
     d_qtown=qtown-qtown0
-    
-    print("ugol %.2f." % ugolserv, "qtown %.3f." % qtown, d_qtown)   
+    #--------------------------------------------------------------
+    print(x,"ugol %.3f." % ugolserv, "qtown %.3f." % qtown, d_qtown)   
     j=0
-    d_trettown=10
-    while j<10 and abs(d_trettown) >0.5: # приращение Т обратки в город меньше 0,5
+    d_trettown = 10
+    d_tinhouse = 0
+    
+    teta0=0
+    d_teta = 1
+    ddt0 = 0
+    
+    while j<20 and d_teta>0.05 : # приращение Т обратки в город меньше 0,5
         
         t_rethouse = ((cwq - btermo/2)*tinhouse + btermo * troom)/(cwq+btermo/2) # обратка из дома - поглощение энергии
     
         d_tmax = tintown + t_rettown
         d_tmin = tinhouse + t_rethouse
-    
+        ddt = (d_tmax - d_tmin)/2
+        
         # энергия которая передается через пластину
-        d_power=atos*d_qtown*(d_tmax - d_tmin)/2
-    
+        d_power=atos*(d_qtown*ddt)
+        
         d_trettown = (d_qtown*(tintown - t_rettown)-d_power/cw)/qtown # прирост Т обр город d_power - прирост энергии d_qtown - прирост расхода
-        t_rettown = t_rettown + d_trettown
+        if t_rettown+d_trettown > tintown:
+            t_rettown = tintown
+        else:    
+            t_rettown = t_rettown + d_trettown
        
         etown=qtown*cw*(tintown - t_rettown)
         ehouse=qhouse*cw*(tinhouse-t_rethouse)
+        #print(' город дoм', etown, ehouse) ------------------------------
+    
         d_tinhouse = (etown-ehouse)/(cw*qhouse) #подача в дом - подсчет разности энергий
-        # подача в дом 
-        tinhouse = tinhouse+d_tinhouse
+        
+        # подача в дом
+        if tinhouse+d_tinhouse > tintown:
+            tinhouse = tintown
+        else:    
+            tinhouse = tinhouse+d_tinhouse
 
-        power1 = cw * qtown * (tintown - t_rettown)
+        teta=((tintown-tinhouse)/2 - (t_rettown-t_rethouse)/2)
+        d_teta=abs(teta-teta0)
+        teta0=teta
+        etown=qtown*cw*(tintown - t_rettown)
+        ehouse=qhouse*cw*(tinhouse-t_rethouse)
         j +=1
-
-        #print('d_power %.2f.'% d_power, 'd_trettown %.3f.'% d_trettown,'power doTO %.2f.'% power1)
-        #print('tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown)
-    print('tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown,'power %.2f.'% power1 )     
+        print(j, teta, d_teta) #-------------------------------------------
+        #print('d_power %.2f.'% d_power, 'd_trettown %.3f.'% d_trettown, 'teta %.3f.' %teta)
+        #print('tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse,'rettown %.2f.' % t_rettown, d_teta)
+    # здесь можно вставить изменение внешних условий по температуре. НЕ слишком резко! ---------------------------------
+    if i<100: #меняем входящую из города
+        tintown=tintown + 0.05
+        if tintown >85 : tintown = 85
+    else :    
+        tintown=tintown - 0.05
+        if tintown < 55 : tintown = 55
+    #-----------------------------------------,'power %.2f.'% etown
+    print('tintown %.2f.' % tintown, ' rettown %.2f.' % t_rettown,'tinhouse %.2f.' % tinhouse, 'rethous %.2f.' % t_rethouse)     
     print(i,'шаг по времени ----------------------')
 print('gameover', tau)    
