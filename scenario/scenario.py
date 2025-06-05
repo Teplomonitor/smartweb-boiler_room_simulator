b5bdf72bc9f23e6ef394244cf674fef640ff1609
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-06-05 18:16:06 +0300

    scenario/scenario.py: working on scenario

--- scenario/scenario.py
+++ scenario/scenario.py
@@ -2,28 +2,70 @@
 @author: admin
 '''
 
-# pip install pytest-aio
+import time
+import threading
 
-import asyncio
+
+def printLog(log_str):
+	print(log_str)
+
+def printError(log_str):
+	print(log_str)
+
+class Scenario_1():
+	def __init__(self, programList):
+		printLog('starting scenario 1')
+		self._programsList = programList
+		self._done = False
+		self._snowmelterProgram = self.getProgramByType('SNOWMELT')
+		if self._snowmelterProgram is None:
+			self._done = True
+			printError('Snowmelter not in program list!')
+	
+	def getProgramByType(self, programType):
+		for program in self._programsList:
+			if program.getType() == programType:
+				return program
+		
+		return None
+		
+	def run(self):
+		if self.done():
+			return
+		pass
+	
+	def done(self):
+		return self._done
 
 
-async def test_async():
-	print('Hello ...')
-	await asyncio.sleep(10)
-	print('... World!')
-	assert False
-
-class Scenario(object):
+class ScenarioThread(threading.Thread):
 	'''
 	classdocs
 	'''
 
 
-	def __init__(self):
+	def __init__(self, controllerHost):
 		'''
 		Constructor
 		'''
+		self._controllerHost = controllerHost
+		self._programsList   = self._controllerHost.getProgramList()
 		
+		self._currentScenario = self.getNextScenario()
+		
+	def getNextScenario(self):
+		return Scenario_1(self._programsList)
 		
 	def run(self):
-		asyncio.run(test_async())
+		while True:
+			self._currentScenario.run()
+			
+			if self._currentScenario.done():
+				self._currentScenario = self.getNextScenario()
+				if self._currentScenario == None:
+					return 0
+				
+			time.sleep(1)
+			
+			
+			
