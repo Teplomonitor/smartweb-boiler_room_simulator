0261b930e275a915ef7276d3f6555a35ac8a442a
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-06-26 20:07:01 +0300

    programs/heating_circuit.py: add test parameter

--- programs/heating_circuit.py
+++ programs/heating_circuit.py
@@ -3,6 +3,7 @@
 '''
 
 from .program import Program
+from gui.parameter import GuiParameter as GuiParameter
 
 class HeatingCircuit(Program):
 	'''
@@ -10,9 +11,13 @@ class HeatingCircuit(Program):
 	'''
 
 
-	def __init__(self, params):
+	def __init__(self, preset):
 		'''
 		Constructor
 		'''
-		super().__init__(params)
+		super().__init__(preset)
 		
+		self._parameters['max_flow_rate'] = GuiParameter(1000, 'Проток')
+		self._parameters['max_flow_rate'].setMin(0)
+		self._parameters['max_flow_rate'].setMax(10000)
+
