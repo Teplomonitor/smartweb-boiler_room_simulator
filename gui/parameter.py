7280b5d41bec0b52352f209661926df85b7a1e6a
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-06-26 19:11:01 +0300

    gui/parameter.py: add new class

---
+++ gui/parameter.py
@@ -0,0 +1,69 @@
+'''
+@author: admin
+'''
+
+class GuiParameter(object):
+	'''
+	classdocs
+	'''
+
+	def __init__(self, value = None, title = None, gui = None):
+		'''
+		Constructor
+		'''
+		self._value   = value
+		self._title   = title
+		self._gui     = gui
+		self._min     =   0
+		self._max     = 100
+		
+	def getValue  (self): return self._value
+	def getTitle  (self): return self._title
+	
+	def setValue  (self, value):
+		self._value = value
+		if self._gui:
+			self._gui.SetValue(value)
+		
+		
+	def setTitle  (self, title  ):
+		self._title = title
+		
+	
+	def onSpin(self, event):
+		event.Skip()
+		self.setValue(self._gui._spinner.GetValue(), True)
+		
+	def onSpinText(self, event):
+		event.Skip()
+		self.setValue(int(self._gui._spinner.GetTextValue()), True)
+		
+	def onScroll(self, event):
+		event.Skip()
+		self.setValue(self._gui._slider .GetValue(), True)
+		
+	def setGui(self, gui):
+		self._gui = gui
+		
+		self.initGui()
+
+	def initGui(self):
+		if self._gui:
+			self._gui.SetMin(self._min)
+			self._gui.SetMax(self._max)
+		
+	def getMin(self): return self._min
+	def getMax(self): return self._max
+	
+	def setMin(self, value):
+		self._min = value
+		
+		if self._gui:
+			self._gui.SetMin(value)
+		
+	def setMax(self, value):
+		self._max = value
+		
+		if self._gui:
+			self._gui.SetMax(value)
+			
