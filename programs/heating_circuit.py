4765bac610e86fbed03cffbf68161c46e47d6cb7
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-06-27 12:37:29 +0300

    programs/heating_circuit.py: update parameter

--- programs/heating_circuit.py
+++ programs/heating_circuit.py
@@ -17,7 +17,12 @@ class HeatingCircuit(Program):
 		'''
 		super().__init__(preset)
 		
-		self._parameters['max_flow_rate'] = GuiParameter(1000, 'Проток')
+		self._parameters['max_flow_rate'] = GuiParameter(1000, 'Расход')
 		self._parameters['max_flow_rate'].setMin(0)
-		self._parameters['max_flow_rate'].setMax(10000)
-
+		self._parameters['max_flow_rate'].setMax(3000)
+		self._parameters['max_flow_rate'].setStep(1)
+		
+		self._parameters['max_flow_rate'].setUnits('кг/ч')
+	
+	def getMaxFlowRate(self):
+		return self._parameters['max_flow_rate'].getValue()
