98200b3dbd06d292e964ca8f308e2f2d30be7f77
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-07-09 18:47:50 +0300

    simulator/district_heating.py: update simulator

--- simulator/district_heating.py
+++ simulator/district_heating.py
@@ -2,6 +2,7 @@
 
 import math
 import time
+import numpy as np
 
 BROADCAST_ID = 0
 
@@ -21,6 +22,71 @@ Gc = G*cw/3600/1000 # kWatt
 
 def limit(lower_bound, value, upper_bound):
 	return max(min(value, upper_bound), lower_bound)
+
+#pdiss=100 # примерно равно мощности отопления
+
+def gss_solver(tintown, pdiss, qtown, qhouse):
+	# Reading number of unknowns
+	n = 3
+	
+	mid_temp=30
+	square=1 # Площадь ТО в кв.м.-----------------------
+	
+	# Making numpy array of n x n+1 size and initializing 
+	# to zero for storing augmented matrix
+	a = np.zeros((n,n+1))
+
+	# Making numpy array of n size and initializing 
+	# to zero for storing solution vector
+	x = np.zeros(n)
+
+	# Reading augmented matrix coefficients
+
+	a[0][0]= -qtown *cw
+	a[0][1]= -qhouse*cw
+	a[0][2]=  qhouse*cw
+	
+	a[0][3]= -qtown*cw*tintown
+
+	a[1][0]= 0
+	a[1][1]=  qhouse*cw-pdiss/2/mid_temp
+	a[1][2]= -qhouse*cw-pdiss/2/mid_temp
+
+	a[1][3]=0
+
+	a[2][0]= -qtown*cw - square * ato/2
+	a[2][1]= square*ato/2
+	a[2][2]= square*ato/2 
+
+	a[2][3]= (square*ato/2 - qtown*cw)*tintown
+
+	# Applying Gauss Elimination
+	for i in range(n):
+		if a[i][i] == 0.0:
+			print('Divide by zero detected!')
+			return
+		
+		for j in range(i+1, n):
+			ratio = a[j][i]/a[i][i]
+		
+			for k in range(n+1):
+				a[j][k] = a[j][k] - ratio * a[i][k]
+
+	# Back Substitution
+	x[n-1] = a[n-1][n]/a[n-1][n-1]
+
+	for i in range(n-2,-1,-1):
+		x[i] = a[i][n]
+	
+		for j in range(i+1,n):
+			x[i] = x[i] - a[i][j]*x[j]
+	
+		x[i] = x[i]/a[i][i]
+	t_rettown = x[0]
+	tinhouse = x[1]
+#	t_rethouse = x[2]
+	# Displaying solution 
+	return t_rettown, tinhouse
 
 class Simulator(object):
 	def __init__(self, program_config, control):
@@ -67,6 +133,7 @@ class Simulator(object):
 		self.t_rettown  = self.getSupplyBackwardTemperature()
 		
 		self.qtown  = 0
+		self._pdiss = 100*1000
 
 	def getSupplyDirectTemperature(self):
 		return self._program.getInputChannel(self._inputId['supply_direct_temp']).getValue()
@@ -113,26 +180,8 @@ class Simulator(object):
 	def getTemperature(self):
 		return self.getDirectTemperature()
 
-	def getCascadePower(self):
-		programList = self._control.getCascadeList()
-		consumerList = []
-		programId = self._program.getId()
-		for program in programList:
-			sourceList = program._program.getPreset().getSettings().getSourceList()
-			if ((programId in sourceList) or
-				(BROADCAST_ID in sourceList) ):
-				consumerList.append(program)
-
-		consumerPower = 0
-		for consumer in consumerList:
-			consumerPower = consumerPower + (consumer.getPower() - self.getPower()) # exclude own district heating power
-
-		return consumerPower
-	
 	def getConsumersPower(self):
-		consumersPower = self._control.getConsumersPower(self._program.getId())
-		cascadePower = self.getCascadePower()
-		return consumersPower + cascadePower
+		return self._control.getConsumersPower(self._program.getId())
 
 
 	def getPumpState(self, pumpId):
@@ -176,6 +225,7 @@ class Simulator(object):
 			tintown    = self.getSupplyDirectTemperature()
 			ugolserv   = valve
 		
+			qtown_max = self.getMaxFlowRateTown()
 			qtown = qtown_max * ugolserv # расход из города при данном угле сервака
 			power = qtown*cw*(tintown-t_rettown) # подсчет текущей мощности теплопередачи ватт
 			
@@ -282,6 +332,15 @@ class Simulator(object):
 			j = j + 1
 			#print('j', j, 'd_teta %.2f.'% d_teta, ddt)	
 		#print('i',i, 'энергия города', etown, ' дом ', ehouse,'----------------------') 
+	
+	def computeQHouse(self):
+		if self.getCirculationPumpState():
+			qhouse = self.getMaxFlowRateHouse()
+		else:
+			qhouse = 0
+		
+		return qhouse
+	
 	def computeTemp(self):
 		valve = self.getValveState()
 		ugolserv = valve
@@ -299,13 +358,38 @@ class Simulator(object):
 		
 		self.qtown = qtown_max * ugolserv * self.getSupplyPumpState()# расход из города при данном угле сервака
 
-		self.mainframe()
+		qhouse = self.computeQHouse()
 		
+		if qhouse == 0:
+			self.t_rettown = self.tintown - 1
 		
+		if self.qtown == 0:
+			self.tinhouse = self.t_rethouse
+			
+		if qhouse == 0 or self.qtown == 0:
+			return
+
+		self._pdiss = self.computePdiss()
 		
+		self.t_rettown, self.tinhouse = gss_solver(self.tintown, self._pdiss, self.qtown, qhouse)
+	
+	def computePdiss(self):
+		qhouse = self.computeQHouse()
+		
+		dT = self.tinhouse - self.t_rethouse
+		pdiss = dT * qhouse * cw
+		
+		dP = self._pdiss - pdiss
+		
+		dP = limit(-1000, dP, 1000)
+		
+		self._pdiss -= dP
+		
+		return self._pdiss
+	
 	def getFlow(self):
 		if self.getCirculationPumpState():
-			return self.getMaxFlowRateHouse() / 1000 # cube per hour
+			return self.getMaxFlowRateHouse() / 1000 * 3600# cube per hour
 		return 0
 	
 	def run(self):
