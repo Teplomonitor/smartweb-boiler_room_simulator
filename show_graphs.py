155d2777fbbe298707e526dea45b396f44f11c48
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-08-08 12:21:41 +0300

    show_graphs.py: display output values as ladder

--- show_graphs.py
+++ show_graphs.py
@@ -26,6 +26,11 @@ def getRandomColor():
 def get_n_last_subparts_path(base_dir: Union[Path, str], n:int) -> Path:
 	return Path(*Path(base_dir).parts[-n-1:])
 
+def valueToPlot(value, valueFormat):
+	if valueFormat == 'RELAY':
+		return float(value)/255*100
+	return float(value)
+	
 def showPlots():
 	ax=plt.gca()
 	ax.xaxis.axis_date()
@@ -41,15 +46,17 @@ def showPlots():
 			filePath = os.path.join(dirPath, file)
 			with open(filePath, encoding='utf-8', newline='') as fp:
 				reader = csv.reader(fp, delimiter=",", quotechar='"')
-				next(reader, None)  # skip the headers
+				header = next(reader, None)  # skip the headers
+				valueFormat = header[1]
 				lines = [row for row in reader]
 				### Create line for every file
 				if len(lines) > 1:
 					x = []
 					y = []
+					
 					for row in lines:
 						x.append(datetime.datetime.fromtimestamp(int(row[0])))
-						y.append(float(row[1]))
+						y.append(valueToPlot(row[1], valueFormat))
 					converted_dates = mdates.date2num(x)
 					
 #					splitext = os.path.splitext(filePath)
@@ -59,9 +66,16 @@ def showPlots():
 #					label = splitext[0]
 					label = str(get_n_last_subparts_path(filePath, 2))
 					
-					plt.plot(converted_dates, y, c=getRandomColor(),
+					if valueFormat == 'RELAY':
+						plt.step(converted_dates, y, c=getRandomColor(),
 						#	linestyle = 'dashed',
+							where = 'post',
 							marker = 'o',
+							label = label)
+					else:
+						plt.plot(converted_dates, y, c=getRandomColor(),
+						#	linestyle = 'dashed',
+							marker = '.',
 							label = label)
 	
 	### Generate the plot
