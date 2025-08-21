1cbde557eb75fc195b977cf57c51e07fbaad62ec
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2025-08-21 12:59:39 +0300

    presets/settings.py: fix heating slope set

--- presets/settings.py
+++ presets/settings.py
@@ -12,7 +12,7 @@ class HeatingCircuitSettings(object):
 	def get(self):
 		return [
 			RemoteControlParameter('CONSUMER', 'GENERATOR_ID', self._source),
-			RemoteControlParameter('CIRCUIT', 'HEATING_SLOPE', int(self._heatingSlope*100)),
+			RemoteControlParameter('CIRCUIT', 'HEATING_SLOPE', self._heatingSlope, parameterType = 'TDP_FLOAT'),
 		]
 	def getSource    (self): return  self._source
 	def getSourceList(self): return [self._source]
