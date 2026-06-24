1419dbac2e92eee204ce2fd8314e78274e392971
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2026-06-24 20:47:11 +0300

    programs/room.py: refactoring

--- programs/room.py
+++ programs/room.py
@@ -16,22 +16,26 @@ class Room(Program):
 	def getType(): return 'ROOM_DEVICE'
 	
 	def initInputs(self):
-		self._inputs['roomTemperature' ] = InputInfo(0, 'Т помещения', -10,   50)
-		self._inputs['mode_deprecated' ] = InputInfo(1, 'Режим'                 )
-		self._inputs['floorTemperature'] = InputInfo(2, 'Т пола'     , -10,   70)
-		self._inputs['wallTemperature' ] = InputInfo(3, 'Т стены'    , -10,   70)
-		self._inputs['humidity'        ] = InputInfo(4, 'Влажность'  ,   0,  100)
-		self._inputs['CO2'             ] = InputInfo(5, 'CO2'        ,   0, 2000)
-		self._inputs['motion'          ] = InputInfo(6, 'Движение'              )
+		self._inputs.update({
+			'roomTemperature' : InputInfo(0, 'Т помещения', -10,   50),
+			'mode_deprecated' : InputInfo(1, 'Режим'                 ),
+			'floorTemperature': InputInfo(2, 'Т пола'     , -10,   70),
+			'wallTemperature' : InputInfo(3, 'Т стены'    , -10,   70),
+			'humidity'        : InputInfo(4, 'Влажность'  ,   0,  100),
+			'CO2'             : InputInfo(5, 'CO2'        ,   0, 2000),
+			'motion'          : InputInfo(6, 'Движение'              ),
+		})
 		
 	def initOutputs(self):
-		self._outputs['valve1'      ] = OutputInfo(0, 'Клапан ТП' )
-		self._outputs['valve2'      ] = OutputInfo(1, 'Клапан РО' )
-		self._outputs['valve3'      ] = OutputInfo(2, 'Клапан ДН' )
-		self._outputs['analogValve1'] = OutputInfo(3, 'Сигнал ТП' )
-		self._outputs['analogValve2'] = OutputInfo(4, 'Сигнал РО' )
-		self._outputs['analogValve3'] = OutputInfo(5, 'Сигнал ДН' )
-		self._outputs['ventilation' ] = OutputInfo(6, 'Вентиляция')
+		self._outputs.update({
+			'valve1'      : OutputInfo(0, 'Клапан ТП' ),
+			'valve2'      : OutputInfo(1, 'Клапан РО' ),
+			'valve3'      : OutputInfo(2, 'Клапан ДН' ),
+			'analogValve1': OutputInfo(3, 'Сигнал ТП' ),
+			'analogValve2': OutputInfo(4, 'Сигнал РО' ),
+			'analogValve3': OutputInfo(5, 'Сигнал ДН' ),
+			'ventilation' : OutputInfo(6, 'Вентиляция'),
+		})
 
 	def initGuiParameters(self):
 		self._parameters['max_power']= GuiParameter(1, 'Мощность', 0, 10, 1, 'кВт')
@@ -55,11 +59,10 @@ class Room(Program):
 		
 		for setting in settings:
 			if setting.getProgramType() == 'ROOM_DEVICE':
-				if setting.getParameterIdCode() == 'RESPONSIBLE_CIRCUIT_1':
-					circuitList[0] = setting.getValue()
-				if setting.getParameterIdCode() == 'RESPONSIBLE_CIRCUIT_2':
-					circuitList[1] = setting.getValue()
-				if setting.getParameterIdCode() == 'RESPONSIBLE_CIRCUIT_3':
-					circuitList[2] = setting.getValue()
+				parameterIdCode = setting.getParameterIdCode()
+				parameterValue  = setting.getValue()
+				if   parameterIdCode == 'RESPONSIBLE_CIRCUIT_1': circuitList[0] = parameterValue
+				elif parameterIdCode == 'RESPONSIBLE_CIRCUIT_2': circuitList[1] = parameterValue
+				elif parameterIdCode == 'RESPONSIBLE_CIRCUIT_3': circuitList[2] = parameterValue
 		return circuitList
 	
