5f8575af36dd6c44e7f2dc19f3179f97e192e3ef
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2026-06-24 20:46:31 +0300

    consoleLog.py: add timestamp to log

--- consoleLog.py
+++ consoleLog.py
@@ -2,6 +2,8 @@
 @author: admin
 '''
 
+import datetime
+
 gui = None
 
 def initGui(guiThread):
@@ -9,14 +11,24 @@ def initGui(guiThread):
 	gui = guiThread
 
 def printLog(log_str):
-	print(log_str)
+	dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	
+	log_str_with_dt = f'{dt}   {log_str}'
+	
+	print(log_str_with_dt)
 	global gui
 	if gui:
-		gui.printConsoleText(log_str)
+#		gui.setTextColor('GREEN')
+		gui.printConsoleText(log_str_with_dt)
 
 def printError(log_str):
-	print(log_str)
+	dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	
+	log_str_with_dt = f'{dt}   {log_str}'
+	
+	print(log_str_with_dt)
 	global gui
 	if gui:
-		gui.printConsoleText(log_str)
+#		gui.setTextColor('RED')
+		gui.printConsoleText(log_str_with_dt)
 		
