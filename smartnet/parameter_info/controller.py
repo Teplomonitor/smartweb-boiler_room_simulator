dba17898db97cc5bdadd777dcb885f5bc36c3f7f
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2026-08-06 16:04:18 +0300

    smartnet/parameter_info/controller.py: fix some controller parameter types

--- smartnet/parameter_info/controller.py
+++ smartnet/parameter_info/controller.py
@@ -6,3 +6,6 @@ PARAMETER_INFO = {
 	parameter_id: {'type': 0}
 	for parameter_id in ControllerParameterId
 }
+
+PARAMETER_INFO[ControllerParameterId.DATE] = {'type': 'DATE'}
+PARAMETER_INFO[ControllerParameterId.TIME] = {'type': 'TIME'}
